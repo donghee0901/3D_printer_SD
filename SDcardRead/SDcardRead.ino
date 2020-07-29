bab28335b95f19f01779b6c85d752e04442f76da
KimDongHee <donghee0901@naver.com>
2020-07-29 21:03:43 +0900

    SD_card_read

---
+++ SDcardRead/SDcardRead.ino
@@ -0,0 +1,82 @@
+#include <SPI.h>
+#include <SD.h>
+File myFile;
+char readData[2] = {'\0', '\0'};
+char readBuffer[100] = {0};
+
+double data_queue[100][3];
+
+char *start_idx, *end_idx;
+char float_buffer[100];
+
+int totalSize = 0;
+int count = 0;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.print("Initializing SD card...");
+  delay(1000);
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  // open the file for reading:
+  
+}
+void loop() {
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      readData[0] = myFile.read();
+      if (readData[0] == '\n') {
+        myFile.close();
+
+        start_idx = strchr(readBuffer, 'X');
+        if (start_idx != NULL) {
+          start_idx = start_idx + 1;
+          end_idx = strchr(start_idx, ' ');
+          if (end_idx != NULL) {
+            strncpy(float_buffer, start_idx, end_idx - start_idx);
+            float_buffer[end_idx - start_idx] = '\0';
+            sscanf(float_buffer, "%lf", &data_queue[count][0]);
+            Serial.print("string x = ");
+            Serial.println(float_buffer);
+            
+            Serial.print("double x = ");
+            Serial.println(data_queue[count][0]);
+          }
+        }
+        start_idx = strchr(readBuffer, 'Y');
+        if (start_idx != NULL) {
+          start_idx = start_idx + 1;
+          end_idx = strchr(start_idx, ' ');
+          if (end_idx != NULL) {
+            strncpy(float_buffer, start_idx, end_idx - start_idx);
+            float_buffer[end_idx - start_idx] = '\0';
+            sscanf(float_buffer, "%lf", &data_queue[count][1]);
+            Serial.print("string y = ");
+            Serial.println(float_buffer);
+            Serial.print("double y = ");
+            Serial.println(data_queue[count][1]);
+          }
+        }
+
+        
+      }
+      strcat(readBuffer, readData);
+
+
+    }
+    // close the file:
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
